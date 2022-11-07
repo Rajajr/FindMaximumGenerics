65de863a3ee10f674635c1b24c1392e178dfd51f
Rajajr <rajjr011@gmail.com>
2022-11-07 07:02:00 +0700

    UC-1 Given 3 Integers Find The Maximum

---
+++ Generics/FindTheMaximum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class FindTheMaximum
+    {
+        public static int FindMaximumIntegerNumber(int firstvalue, int secondvalue, int thridvalue)
+        {
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thridvalue) > 0)
+            {
+                return firstvalue;
+            }
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thridvalue) > 0)
+            {
+                return secondvalue;
+            }
+            if (thridvalue.CompareTo(firstvalue) > 0 && thridvalue.CompareTo(secondvalue) > 0)
+            {
+                return thridvalue;
+            }
+            throw new Exception("firstNumber,secondNumber and thridNumber are same");
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Programe");
+            FindTheMaximum findTheMaximum = new FindTheMaximum();
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("1:Find The Maximum Integer Number Among 20, 7, 3");
+            Console.WriteLine("Maximum Integer Number is :" + FindTheMaximum.FindMaximumIntegerNumber(20, 7, 3));
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("2:Find The Maximum Integer Number Among 2, 15, 4");
+            Console.WriteLine("Maximum Integer Number is :" + FindTheMaximum.FindMaximumIntegerNumber(2, 15, 4));
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("3:Find The Maximum Integer Number Among 11, 7, 9");
+            Console.WriteLine("Maximum Integer Number is :" + FindTheMaximum.FindMaximumIntegerNumber(11, 7, 9));
+
+            Console.ReadLine();
+        }
+    }
+}
