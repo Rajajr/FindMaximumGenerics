2b0a7cfbfc6e33cdb3726593980b5ca6923b2fd7
Rajajr <rajjr011@gmail.com>
2022-11-07 18:50:22 +0700

    UC-2 Given 3 Floats Find the Maximum Floats

---
+++ Generics/FindMaximumFloat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class FindMaximumFloat
+    {
+        public static float FindMaximumFloatNumber(float firstvalue, float secondvalue, float thridvalue)
+        {
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thridvalue) > 0)
+            {
+                return firstvalue;
+            }
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thridvalue) > 0)
+            {
+                return secondvalue;
+            }
+            if (thridvalue.CompareTo(firstvalue) > 0 && thridvalue.CompareTo(secondvalue) > 0)
+            {
+                return thridvalue;
+            }
+            throw new Exception("firstNumber,secondNumber and thridNumber are same");
+        }
+    }
+}
--- Generics/Program.cs
+++ Generics/Program.cs
@@ -11,16 +11,29 @@ namespace Generics
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Programe");
-            FindTheMaximum findTheMaximum = new FindTheMaximum();
+            //  FindTheMaximum findTheMaximum = new FindTheMaximum();
+            FindMaximumFloat findMaximumFloat = new FindMaximumFloat();
+            //  Console.WriteLine("--------------------------------------");
+            //  Console.WriteLine("1:Find The Maximum Integer Number Among 20, 7, 3");
+            //  Console.WriteLine("Maximum Integer Number is :" + FindTheMaximum.FindMaximumIntegerNumber(20, 7, 3));
+
+            // Console.WriteLine("--------------------------------------");
+            // Console.WriteLine("2:Find The Maximum Integer Number Among 2, 15, 4");
+            // Console.WriteLine("Maximum Integer Number is :" + FindTheMaximum.FindMaximumIntegerNumber(2, 15, 4));
+            // Console.WriteLine("--------------------------------------");
+            // Console.WriteLine("3:Find The Maximum Integer Number Among 11, 7, 9");
+            // Console.WriteLine("Maximum Integer Number is :" + FindTheMaximum.FindMaximumIntegerNumber(11, 7, 9));
+
             Console.WriteLine("--------------------------------------");
-            Console.WriteLine("1:Find The Maximum Integer Number Among 20, 7, 3");
-            Console.WriteLine("Maximum Integer Number is :" + FindTheMaximum.FindMaximumIntegerNumber(20, 7, 3));
+            Console.WriteLine("1:Find The Maximum Float Number Among 3.56, 10.78, 5.50");
+            Console.WriteLine("Maximum Float Number is :" + FindMaximumFloat.FindMaximumFloatNumber(3.56f, 10.78f, 5.50f));
+
             Console.WriteLine("--------------------------------------");
-            Console.WriteLine("2:Find The Maximum Integer Number Among 2, 15, 4");
-            Console.WriteLine("Maximum Integer Number is :" + FindTheMaximum.FindMaximumIntegerNumber(2, 15, 4));
+            Console.WriteLine("2:Find The Maximum Float Number Among 2.23, 8.45, 4.56");
+            Console.WriteLine("Maximum Float Number is :" + FindMaximumFloat.FindMaximumFloatNumber(2.23f, 8.45f, 4.56f));
             Console.WriteLine("--------------------------------------");
-            Console.WriteLine("3:Find The Maximum Integer Number Among 11, 7, 9");
-            Console.WriteLine("Maximum Integer Number is :" + FindTheMaximum.FindMaximumIntegerNumber(11, 7, 9));
+            Console.WriteLine("3:Find The Maximum Float Number Among 1.49, 7.23, 9.16");
+            Console.WriteLine("Maximum Float Number is :" + FindMaximumFloat.FindMaximumFloatNumber(1.49f, 7.23f, 9.16f));
 
             Console.ReadLine();
         }
